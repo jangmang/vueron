68ad6f286af333ecf7f93630329569e268d33475
jmh <zzzmh@naver.com>
2021-03-28 16:12:53 +0900

    210328

--- static/js/common.js
+++ static/js/common.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 		autoScrolling: true,
         // loopHorizontal: false,
 		onLeave: function(index, nextIndex, direction){
-			if(nextIndex == 2){
+			// section1_2 메뉴활성화
+			if(nextIndex == 2){ 
 				$("#gnb li").eq(0).find("a").addClass("on");			
 				$("#indicator li").eq(0).find("a").addClass("on");			
 			} else {
@@ -14,26 +15,63 @@ $(document).ready(function() {
 				$("#indicator li").eq(0).find("a").removeClass("on");		
 			}
 
-			if($(".sec2 .sec_inner").hasClass("focusMove")){
-				$(".sec2 .sec_inner").removeClass("focusMove");
+			// sec2 view2
+			if($(".sec2").hasClass("view2")){
+				setTimeout(function(){
+					$(".sec2").removeClass("view2");
+				},500);
 			}
 		}
 	});
 	
-
 	// 	$(".btn_contact").on("click", function(){
-	//         $.fn.fullpage.moveTo(6);
+	//  	$.fn.fullpage.moveTo(6);
 	// 	});
-	$(".sec2 .banner_area li a").on("click", function(){
-		$(".sec2 .sec_inner").addClass("focusMove");
+
+	// sec1_1 
+	$(".sec1_1 .btn_more").on("click", function(){
+		$.fn.fullpage.moveTo(2);
 	});
 
+	// sec2
+	$(".sec2 .banner_area li a").on("click", function(){
+		$(".sec2").addClass("view2");
+	});
 	$('.sec2_slider').slick({
-		fade: true,
 		arrows: true,
 		infinite: false,	
 	});
 	$('.sec2_slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
 		player1.cueVideoById(youtubeId1[currentSlide]);
 	});	
+
+	// sec3
+	$('.sec3_slider').slick({
+		arrows: true,
+		infinite: false,	
+		prevArrow: $(".sec3 .btn_prev"),
+        nextArrow: $(".sec3 .btn_next"), 
+	});
+	$('.sec3_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		player2.cueVideoById(youtubeId2[nextSlide]);
+		$(".sec3 .view_menu li a").removeClass("on");
+		$(".sec3 .view_menu li").eq(nextSlide).find("a").addClass("on");
+	});	
+	$(".sec3 .view_menu li a").on("click", function(){
+		var idx = $(this).parent().index();
+		$(".sec3 .view_menu li a").removeClass("on");
+		$(this).addClass("on");
+		$('.sec3_slider').slick('slickGoTo', idx);
+	});
+
+	// sec4
+	$(".sec4 .btn_more").on("click", function(){
+		$.fn.fullpage.moveTo(6);
+	});
+
+	// partners_banner_slider
+	$('.partners_banner_slider').slick({
+		arrows: true,
+		infinite: false,	
+	});
 });
--- static/js/youtube_api.js
+++ static/js/youtube_api.js
@@ -1,15 +1,12 @@
 var player1, player2;
 
 var youtubeId1 = ["EDtKd32jVbo", "EDtKd32jVbo", "EDtKd32jVbo"];
-var youtubeId2 = ["HRH-dvClp5A", "ISJdQ4e1Wzo"];
+var youtubeId2 = ["EDtKd32jVbo", "EDtKd32jVbo", "EDtKd32jVbo"];
 
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var video2_isplay = false;
-var video2_readyPlay = false;
 
 function onYouTubePlayerAPIReady() {
 	player1 = new YT.Player("sec2_vide_1", {
@@ -22,7 +19,7 @@ function onYouTubePlayerAPIReady() {
 			'onStateChange': onPlayerStateChange1
 		}
 	});
-    player2 = new YT.Player("player2", {
+    player2 = new YT.Player("sec3_vide_1", {
 		height: '100%',
 		width: '100%',
 		videoId: youtubeId2[0],
@@ -38,55 +35,19 @@ function onPlayerReady1(event) {
 	event.target.setPlaybackQuality('hd720');     
 }
 function onPlayerReady2(event) {
-	event.target.setPlaybackQuality('hd720');
-    video2_readyPlay = true;        
+	event.target.setPlaybackQuality('hd720');     
 }
 
 function onPlayerStateChange1(event) {
     if (event.data == YT.PlayerState.PLAYING) {
-        // player2.pauseVideo();
+        player2.pauseVideo();
     }	
 }
 function onPlayerStateChange2(event) {	
 	if (event.data == YT.PlayerState.PLAYING) {
-        video2_isplay = true;
-        // player1.pauseVideo();
-    } else {
-        video2_isplay = false;
-    }
-	if(event.data == YT.PlayerState.ENDED) {
-        video2_isplay = false;
-	}
+        player1.pauseVideo();
+    } 
 }
-
-$(function(){
-    // //영상탭메뉴
-    // $(".video2_area .video_thumb a").on("click", function(){   
-    //     if(!video2_readyPlay) return false;   
-    //     $(this).each(function(){
-    //         var idx = $(this).parent().index();  
-    //         if(!$(this).hasClass("on")){
-    //             player2.cueVideoById(youtubeId2[idx]);                
-    //             player2.playVideo(); 
-    //         } else {
-    //             if(!video2_isplay) player2.playVideo();
-    //         }            
-    //     });   
-    //     $(".video2_area .video_thumb a").removeClass("on");
-    //     $(this).addClass("on");             
-    //     $(".video2_area .poster").hide();
-    // });
-
-    // //영상커버버튼
-    // $(".poster").on("click", function(){
-    //     if($(this).parent().hasClass('video1_area')) {
-    //         player1.playVideo();  
-    //     } else {
-    //         player2.playVideo(); 
-    //     }
-    //     $(this).hide();              
-    // }); 
-});
 
 
 
